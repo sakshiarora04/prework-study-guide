9f7662e43e5e5c07fd259a7fc6ad4a243875166b
sakshiarora04 <sakshiarora245@gmail.com>
2023-05-08 15:45:14 +1000

    add code in javascript file

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,30 @@
+var topics = "HTML, CSS, Git, JavaScript";
+console.log(topics);
+const foo =42;
+const result = foo+"1";
+console.log(result);
+if (0) {
+    console.log("I'm 0!")
+ } else if (null) {
+    console.log("I'm null!")
+ } else if (true) {
+    console.log("I'm true!")
+ }
+ /*var topic = "HTML";
+
+ if (topic === 'HTML') {
+   console.log("Let's study HTML!");
+ } else if (topic === 'CSS') {
+   console.log("Let's study CSS!");
+ } else if (topic === 'Git') {
+   console.log("Let's study Git!");
+ } else if (topic === 'JavaScript') {
+   console.log("Let's study JavaScript!");
+ } else {
+   console.log('Please try again!');
+ }*/
+ var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (var x = 0; x < topics.length; x++) {
+ console.log(topics[x]);
+}
